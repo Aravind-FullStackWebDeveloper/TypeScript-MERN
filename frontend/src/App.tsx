d752354fe7341ac439699ec7a34e1468b47bb30e
Aravind Reddy Patlolla <Aravindreddy2508@outlook.com>
2023-12-21 00:38:03 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <div className="app">
+        <header>
+          TS Amazon clone
+        </header>
+        <main>
+          Main section
+        </main>
+        <footer>
+          All rights reserved
+        </footer>
+      </div>
+            </>
+  )
+}
+
+export default App
---
+++ frontend/src/README.md
@@ -0,0 +1,8 @@
+#Type Script mern api
+
+#Lessons
+
+1. Introduction
+2. Install tools
+3. create Typescript React by by VITE
+4. create Git repository
